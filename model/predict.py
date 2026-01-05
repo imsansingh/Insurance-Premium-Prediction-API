b0e11e9ef6a580fe0b4bf5cd6fab95979da2b546
Sandeep Singh Rajawat <145151533+imsansingh@users.noreply.github.com>
2026-01-05 10:43:11 +0530

    Create predict.py

---
+++ model/predict.py
@@ -0,0 +1,31 @@
+import pickle
+import pandas as pd
+
+# import the ml model
+with open('model/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+MODEL_VERSION = "1.0.0"
+
+# Get class labels from the model( important for matching possibilities to class names)
+class_lables = model.classes_.tolist()
+
+def predict_output(user_input: dict):
+    
+    df = pd.DataFrame([user_input])
+
+    # predict the class
+    predicted_class = model.predict(df)[0]
+
+    # get probabilities for each class
+    probabilities = model.predict_proba(df)[0]
+    confidence = max(probabilities)
+
+    # create mapping: {class_name: probability}
+    class_probs = dict(zip(class_lables, map(lambda p: round(p, 4), probabilities)))
+
+    return {
+        'predicted_category ': predicted_class,
+        'confidence': round(confidence, 4), 
+        'class_probabilities': class_probs
+    }
